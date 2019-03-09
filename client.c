6a30768b6cf7da8737fa5a68008b0ceec4cabd0a
johnnylord <johnnylord7410@gmail.com>
2019-03-09 17:21:33 +0800

    Measure the time to calculate fibnacci number

--- client.c
+++ client.c
@@ -3,14 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci"
 
 int main()
 {
-    int fd;
+    clockid_t cid = 1;
+    struct timespec start, end;
+
+    int fd, fd_out;
     long long sz;
+    long int sec, nanosec;
 
     char buf[1];
     char write_buf[] = "testing writing";
@@ -18,6 +23,7 @@ int main()
     int i = 0;
 
     fd = open(FIB_DEV, O_RDWR);
+    fd_out = open("output.txt", O_RDWR);
 
     if (fd < 0) {
         perror("Failed to open character device");
@@ -29,9 +35,19 @@ int main()
         printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
     }
 
+    char buffer[100];
+
     for (i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
+
+        clock_gettime(cid, &start);
         sz = read(fd, buf, 1);
+        clock_gettime(cid, &end);
+        sec = end.tv_sec - start.tv_sec;
+        nanosec = end.tv_nsec - start.tv_nsec;
+        sprintf(buffer, "%d %ld\n", i, sec * 1000000000 + nanosec);
+        write(fd_out, buffer, strlen(buffer));
+
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
                "%lld.\n",
@@ -48,5 +64,6 @@ int main()
     }
 
     close(fd);
+    close(fd_out);
     return 0;
 }
